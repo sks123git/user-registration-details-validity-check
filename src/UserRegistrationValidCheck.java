ad9e07b85f119456f05c70cdb31b66a929355604
subzero <shubham8111994@gmail.com>
2023-03-01 01:59:25 +0530

    added: validation feature for user last name

--- src/UserRegistrationValidCheck.java
+++ src/UserRegistrationValidCheck.java
@@ -3,7 +3,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistrationValidCheck {
-
     public void getFirstName(){
         UserRegistrationValidCheck user=new UserRegistrationValidCheck();
         Scanner scanner=new Scanner(System.in);
@@ -17,11 +16,22 @@ public class UserRegistrationValidCheck {
             user.getFirstName();
         }
     }
-
-
-
+    public void getLastName(){
+        UserRegistrationValidCheck user=new UserRegistrationValidCheck();
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the Last name");
+        UserDetails userDetails=new UserDetails(scanner.nextLine());
+        Pattern pattern=Pattern.compile("([A-Z][a-z]{2,})");
+        Matcher matcher=pattern.matcher(userDetails.firstName);
+        boolean m=matcher.matches();
+        if(!m) {
+            System.out.println("Please enter 1st letter in caps and min 3 letters");
+            user.getLastName();
+        }
+    }
     public static void main(String[] args) {
         UserRegistrationValidCheck user=new UserRegistrationValidCheck();
         user.getFirstName();
+        user.getLastName();
     }
 }
